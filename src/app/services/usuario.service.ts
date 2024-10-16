05f880ddf6b13f7468b221fbb415c6e5ce682a71
Wilcorfis <wilmerandres505@gmail.com>
2024-10-16 16:59:49 -0500

    ss

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -9,22 +9,22 @@ export class UsuarioService {
   private http=inject(HttpClient);
 
   list(){
-    return this.http.get<Usuario[]>('http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios')
+    return this.http.get<Usuario[]>('https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios')
   }
   get(id: number){
-    return this.http.get<Usuario>('http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}');
+    return this.http.get<Usuario>('https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}');
   }
   create(usuario: any){
-    return this.http.post<Usuario>('http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios',usuario)
+    return this.http.post<Usuario>('https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios',usuario)
   }
   update(id: number,usuario: any){
-    return this.http.put<Usuario>('http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}',usuario)
+    return this.http.put<Usuario>('https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}',usuario)
   }
   delete(id:number){
-    return this.http.delete<void>('http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}')
+    return this.http.delete<void>('https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios/${id}')
   }
-///localhost:8080
-//http://new-christen-wilcorfis-23727a02.koyeb.app/usuarios
+///http://localhost:8080
+//https://new-christen-wilcorfis-23727a02.koyeb.app/usuarios
 //https
 
 }
