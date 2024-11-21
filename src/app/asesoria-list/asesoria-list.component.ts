c3c9a0bca747e307457ed6ad288049977cad104d
Wilcorfis <wilmerandres505@gmail.com>
2024-11-20 21:13:10 -0500

    filtro de busqueda

--- src/app/asesoria-list/asesoria-list.component.ts
+++ src/app/asesoria-list/asesoria-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UsuarioService } from '../services/usuario.service';
 import { AsesoriaService } from '../services/asesoria.service';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Usuario } from '../model/usuario.interface';
@@ -8,18 +9,21 @@ import { Asesoria } from '../model/asesoria.interface';
 import { AuthService } from '../services/auth.service';
 import { SuscripcionasesoriaService } from '../services/suscripcionasesoria.service';
 import { Suscripcionasesoria } from '../model/suscripcionasesoria.interface';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
   selector: 'app-asesoria-list',
   standalone: true,
-  imports: [DatePipe,RouterModule],
+  imports: [DatePipe,RouterModule,ReactiveFormsModule],
   templateUrl: './asesoria-list.component.html',
   styleUrl: './asesoria-list.component.css'
 })
 export default class AsesoriaListComponent implements OnInit {
   private usuarioService=inject(UsuarioService);
   asesoria:any[]=[];
+  filteredAsesorias: any[] = [];
+  filterForm: FormGroup;
   suscripcion:any | null=null
 
   
@@ -29,8 +33,16 @@ export default class AsesoriaListComponent implements OnInit {
 
   constructor(private authService: AuthService,
     private asesoriaService :AsesoriaService,
-    private suscripcionService:SuscripcionasesoriaService
-  ) {}
+    private suscripcionService:SuscripcionasesoriaService,
+    private fb: FormBuilder
+  ) {
+    this.filterForm = this.fb.group({
+      tutor: [''],
+      asignatura: [''],
+      fechaDesde: [''], // Fecha inicial del rango
+      fechaHasta: [''], // Fecha final del rango
+    });
+  }
  
   
 
@@ -40,7 +52,31 @@ export default class AsesoriaListComponent implements OnInit {
 
 
     this.loadAll();
+    this.filterForm.valueChanges.subscribe((filters) => {
+      this.applyFilters(filters);
+    });
 
+  }
+  applyFilters(filters: any): void {
+    this.filteredAsesorias = this.asesoria.filter((asesoria) => {
+      const matchesTutor =
+        !filters.tutor ||
+        `${asesoria.tutor.primer_nombre} ${asesoria.tutor.primer_apellido}`
+          .toLowerCase()
+          .includes(filters.tutor.toLowerCase());
+      const matchesAsignatura =
+        !filters.asignatura ||
+        asesoria.asignatura.nombre.toLowerCase().includes(filters.asignatura.toLowerCase());
+
+      const matchesFechaDesde =
+        !filters.fechaDesde ||
+        new Date(asesoria.fecha_asesoria) > new Date(filters.fechaDesde);
+      const matchesFechaHasta =
+        !filters.fechaHasta ||
+        new Date(asesoria.fecha_asesoria) <= new Date(filters.fechaHasta);
+
+      return matchesTutor && matchesAsignatura && matchesFechaDesde && matchesFechaHasta;
+    });
   }
   obtenerUsuario(): void {
     // Verificar si ya tenemos el usuario en localStorage
@@ -74,6 +110,7 @@ export default class AsesoriaListComponent implements OnInit {
     this.asesoriaService.list()
     .subscribe(asesorias=>{
       this.asesoria=asesorias;
+      this.filteredAsesorias =asesorias;
       this.suscripcionService.getbyidusuario(idUsuario).subscribe(suscri=>{
         this.suscripcion=suscri;
       }
